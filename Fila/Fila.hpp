03d27519c7ffcb526b0a25a953ebff89ee058d8b
caiopo <caiopoliveira@gmail.com>
2016-04-05 02:32:40 -0300

    Pilha e Fila finalizadas

--- Fila/Fila.hpp
+++ Fila/Fila.hpp
@@ -5,13 +5,22 @@
 
 #include <stdexcept>
 
-
+/**
+ * @brief      Implementa uma estrutura de dados do tipo Fila,
+ *			   que organiza os elementos de forma que o primeiro a ser inserido
+ *			   é o primeiro a ser retirado (First In First Out ou FIFO).
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ */
 template <typename T>
 class Fila {
  private:
-	static const int DEFAULT_SIZE = 10;
-	T* dados;
-	int tam, inicio, fim;
+	static const int DEFAULT_SIZE = 10;  // Tamanho padrão caso o parâmetro t
+										 // do construtor não seja especificado
+	int MAXFILA; /* Tamanho máximo da pilha, definido na instanciação da classe */
+	int inicio; /* Guarda o índice do elemento no inicio da fila */
+	int fim; /* Guarda o índice do elemento no final da fila */
+	T* dados; /* Guarda os dados inseridos na pilha */
 
  public:
 	explicit Fila(int t = DEFAULT_SIZE);
@@ -24,61 +33,119 @@ class Fila {
 	void inicializaFila();
 };
 
+/**
+ * @brief      Construtor da classe Fila.
+ *
+ * @param[in]  t     Tamanho máximo da fila
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ */
 template <typename T>
 Fila<T>::Fila(int t) {
 	dados = new T[t];
-	tam = t;
+	MAXFILA = t;
 	inicio = -1;
 	fim = -1;
 }
 
+/**
+ * @brief      Adiciona um elemento no final da fila.
+ *			   Joga a exceção "std::runtime_error" caso a fila esteja cheia.
+ *
+ * @param[in]  dado  Dado para ser adicionado no final da fila
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ */
 template <typename T>
 void Fila<T>::inclui(T dado) {
-	if(filaCheia())
+	if (filaCheia())
 		throw std::runtime_error("fila cheia");
 
-	fim = (fim+1) % tam;
+	fim = (fim+1) % MAXFILA;
 
 	dados[fim] = dado;
 }
 
+/**
+ * @brief      Retira o elemento no início da fila e o retorna.
+ *			   Joga a exceção "std::runtime_error" caso a fila esteja vazia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     O elemento retirado do início da fila
+ */
 template <typename T>
 T Fila<T>::retira() {
-	if(filaVazia())
+	if (filaVazia())
 		throw std::runtime_error("fila vazia");
 
-	inicio = (inicio+1) % tam;
+	inicio = (inicio+1) % MAXFILA;
 
 	return dados[inicio];
 }
 
+/**
+ * @brief      Retorna o elemento no início da fila sem remove-lo. Joga a
+ *             exceção "std::runtime_error" caso a fila esteja vazia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     O elemento no final da fila
+ */
 template <typename T>
 T Fila<T>::ultimo() {
-	if(filaVazia())
+	if (filaVazia())
 		throw std::runtime_error("fila vazia");
 	return dados[fim];
 }
 
+/**
+ * @brief      Retorna o índice do último elemento da fila. Joga a exceção
+ *             "std::runtime_error" caso a fila esteja vazia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     O índice do elemento no final da fila
+ */
 template <typename T>
 int Fila<T>::getUltimo() {
-	int size = abs(fim-inicio);
-
-	if(size == 0)
+	if (filaVazia())
 		throw std::runtime_error("fila vazia");
+	else if (fim < inicio)
+		return fim + (MAXFILA - inicio) - 1;
 	else
-		return size-1;
+		return fim - inicio - 1;
 }
 
+/**
+ * @brief      Testa se a fila está cheia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     True caso a fila esteja cheia, False caso contrário
+ */
 template <typename T>
 bool Fila<T>::filaCheia() {
-	return abs(fim-inicio) == tam;
+	return abs(fim-inicio) == MAXFILA;
 }
 
+/**
+ * @brief      Testa se a fila está vazia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     True caso a fila esteja vazia, False caso contrário
+ */
 template <typename T>
 bool Fila<T>::filaVazia() {
 	return inicio == fim;
 }
 
+/**
+ * @brief      Limpa a fila, removendo todos os elementos.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ */
 template <typename T>
 void Fila<T>::inicializaFila() {
 	inicio = -1;
--- Pilha/Pilha.hpp
+++ Pilha/Pilha.hpp
@@ -5,86 +5,145 @@
 
 #include <stdexcept>
 
+/**
+ * @brief      Implementa uma estrutura de dados do tipo Pilha,
+ *			   que organiza os elementos de forma que o último a ser inserido
+ *			   é o primeiro a ser retirado (Last In First Out ou LIFO).
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ */
 template<typename T>
 class Pilha {
-	private:
-		int MAXPILHA;
-		int size;
-		T* dados;
+ private:
+	static const int DEFAULT_SIZE = 10;  // Tamanho padrão caso o parâmetro t
+										 // do construtor não seja especificado
+	int MAXPILHA; /* Tamanho máximo da pilha, definido na instanciação da classe */
+	int _topo; /* Guarda o índice do elemento no topo da pilha */
+	T* dados; /* Guarda os dados inseridos na pilha */
 
-	public:
-		Pilha();
-		Pilha<T>(int t);
-		void empilha(T dado);
-		T desempilha();
-		T topo();
-		int getPosTopo();
-		void limparPilha();
-		bool PilhaVazia();
-		bool PilhaCheia();
+ public:
+	explicit Pilha<T>(int t = DEFAULT_SIZE);
+	void empilha(T dado);
+	T desempilha();
+	T topo();
+	int getPosTopo();
+	void limparPilha();
+	bool PilhaVazia();
+	bool PilhaCheia();
 };
 
-template <typename T>
-Pilha<T>::Pilha() {
-	MAXPILHA = 10;
-	dados = new T[10];
-	size = -1;
-}
-
+/**
+ * @brief      Construtor da classe Pilha.
+ *
+ * @param[in]  t     Tamanho máximo da pilha
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ */
 template <typename T>
 Pilha<T>::Pilha(int t) {
 	MAXPILHA = t;
 	dados = new T[t];
-	size = -1;
+	_topo = -1;
 }
 
+/**
+ * @brief      Insere um novo elemento no topo da pilha.
+ *			   Joga a exceção "std::runtime_error" caso a pilha esteja cheia.
+ *
+ * @param[in]  dado  Elemento para inserir
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ */
 template <typename T>
 void Pilha<T>::empilha(T dado) {
 	if(PilhaCheia())
 		throw std::runtime_error("pilha cheia");
 
-	size++;
-	dados[size] = dado;
+	_topo++;
+	dados[_topo] = dado;
 }
 
+/**
+ * @brief      Retira o elemento no topo da pilha e o retorna.
+ *			   Joga a exceção "std::runtime_error" caso a pilha esteja vazia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     O elemento que estava no topo da pilha
+ */
 template <typename T>
 T Pilha<T>::desempilha() {
 	if(PilhaVazia())
 		throw std::runtime_error("pilha vazia");
 
-	size--;
-	return dados[size+1];
+	_topo--;
+	return dados[_topo+1];
 }
 
+/**
+ * @brief      Olha o elemento no topo da pilha e o retorna, sem retirá-lo.
+ *			   Joga a exceção "std::runtime_error" caso a pilha esteja vazia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     O elemento que está no topo da pilha
+ */
 template <typename T>
 T Pilha<T>::topo() {
 	if(PilhaVazia())
 		throw std::runtime_error("pilha vazia");
 
-	return dados[size];
+	return dados[_topo];
 }
 
+/**
+ * @brief      Consulta o valor do índice do elemento no topo da pilha,
+ * 			   este valor, somado a um corresponde ao tamanho atual da pilha.
+ * 			   Joga a exceção "std::runtime_error" caso a pilha esteja vazia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     O valor do índice do elemento no topo da pilha
+ */
 template <typename T>
 int Pilha<T>::getPosTopo() {
-	if(size == -1)
-		throw std::runtime_error("topo inexistente: pilha vazia");
-	return size;
+	if(_topo == -1)
+		throw std::runtime_error("pilha vazia");
+	return _topo;
 }
 
+/**
+ * @brief      Retira todos os elementos da pilha, voltando ao estado de pilha vazia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ */
 template <typename T>
 void Pilha<T>::limparPilha() {
-	size = -1;
+	_topo = -1;
 }
 
-
+/**
+ * @brief      Testa se a pilha está cheia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     True caso a pilha esteja cheia, False caso contrário
+ */
 template <typename T>
 bool Pilha<T>::PilhaCheia() {
-	return size == MAXPILHA-1;
+	return _topo == MAXPILHA-1;
 }
 
+/**
+ * @brief      Testa se a pilha está vazia.
+ *
+ * @tparam     T     Parâmetro genérico de Template
+ *
+ * @return     True caso a pilha esteja vazia, False caso contrário
+ */
 template <typename T>
 bool Pilha<T>::PilhaVazia() {
-	return size == -1;
+	return _topo == -1;
 }
 
 #endif  // PILHA_HPP
